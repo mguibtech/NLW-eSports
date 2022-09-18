f81098b4a397f7977d103f876f82a1b729bcb4c2
Marcos Guibson <mguibtech@gmail.com>
2022-09-17 21:37:14 -0300

    Finalizando estilização da aplicação web

---
+++ web/src/components/Form/Input.tsx
@@ -0,0 +1,12 @@
+import {InputHTMLAttributes} from 'react'
+
+interface InputPorps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function Input(props: InputPorps) {
+  return (
+    <input
+      {...props}
+      className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+    />
+  )
+}
